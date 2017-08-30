96ed013650835715f29696d29c136ea258c18c8f
shodaisuzuki <shodai-connect@tbz.t-com.ne.jp>
2017-08-30 12:08:34 +0900

    認証確認フィルター実装 & ログイン後画面を変更
    ログイン後画面をindexからshowに変更

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,11 +1,13 @@
 class UserSessionsController < ApplicationController
+  skip_before_action :require_login, except: [:destroy]
+
   def new
     @user = User.new
   end
 
   def create
     if @user = login(params[:email], params[:password])
-      redirect_back_or_to(:users, notice: "login successful")
+      redirect_back_or_to(user_path(@user), notice: "login successful")
     else
       flash.now[:alert] = "login feiled"
       render action: 'new'
