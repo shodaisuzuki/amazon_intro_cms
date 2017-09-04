f215eb8bbab36df509d2148f1b513a49564aecd2
shodaisuzuki <shodai-connect@tbz.t-com.ne.jp>
2017-09-04 17:42:28 +0900

    product実装開始
    一覧画面とroutesだけ

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :set_site
 
   # GET /products
   # GET /products.json
@@ -65,6 +66,10 @@ class ProductsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
+    end
+
+    def set_site
+      @site = Site.find(params[:site_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
